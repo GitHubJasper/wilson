fb0303671151c35caa5aa6ecbd77e83c51bfe8a9
mlrprananta <lukeprananta@hotmail.com>
2017-10-14 10:37:13 +0200

    Add new bot reaction and new files

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require("./auth.json");
+//const sql = require("sqlite");
+
 
 client.on('ready', function (evt) {
     console.log('Connected');
@@ -12,6 +14,14 @@ client.on('message', function(message) {
     };
    if (message.content.toUpperCase() === 'GEODUDE') {
        message.channel.send('https://i.imgur.com/YdVoIqp.png');
+   }
+   if (message.content.toUpperCase() === 'PRAISE BE') {
+       let embed = new Discord.RichEmbed();
+       embed
+           .setColor(3447003)
+           .setTitle("Me RNGsus")
+           .setImage('https://s3.amazonaws.com/files.d20.io/images/33679087/1OxkRToSEBz_UbvRVrGp9A/med.jpg?1495996223971');
+       message.channel.send(embed);
    }
 });
 
