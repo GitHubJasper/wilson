bfc1429d1dca7f4b1f8be612608f8fedabffcb28
Jasper van Esveld <jaspervanesveld@gmail.com>
2017-11-04 15:43:57 +0100

    Remove async from weather
    Async is not included in older nodejs version which is installed on the server

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -3,31 +3,25 @@ const weather = require("yahoo-weather");
 
 module.exports.run = function(client, message, args) {
     let city = args[0];
-    const request = async () => {
-        try {
-            let info = await weather(city);
-            if (info) {
-                let embed = new Discord.RichEmbed();
-                embed
-                    .setTitle(`Weather in ${info.location.city}, ${info.location.country}`)
-                    .setURL(info.item.link.split('*')[1])
-                    .setDescription(`${info.item.title}`)
-                    .setFooter(`Weather data obtained from ${info.image.title}`)
-                    .addField('Maximum', `${info.item.forecast[0].high} °${info.units.temperature}`, true)
-                    .addField('Minimum', `${info.item.forecast[0].low} °${info.units.temperature}`, true)
-                    .addField('Recent', `${info.item.condition.temp} °${info.units.temperature}`, true)
-                    .addField('Wind Speed', `${info.wind.speed} ${info.units.speed}`, true)
-                    .addField('Humidity', `${info.atmosphere.humidity}%`, true)
-                    .addField('Condition', info.item.condition.text, true);
-                message.channel.send(embed);
-            } else {
-                message.channel.send("No data!");
-            }
-        } catch (err) {
-            console.error(err);
+    weather(city).then(info => {
+        if (info) {
+            let embed = new Discord.RichEmbed();
+            embed
+                .setTitle(`Weather in ${info.location.city}, ${info.location.country}`)
+                .setURL(info.item.link.split('*')[1])
+                .setDescription(`${info.item.title}`)
+                .setFooter(`Weather data obtained from ${info.image.title}`)
+                .addField('Maximum', `${info.item.forecast[0].high} °${info.units.temperature}`, true)
+                .addField('Minimum', `${info.item.forecast[0].low} °${info.units.temperature}`, true)
+                .addField('Recent', `${info.item.condition.temp} °${info.units.temperature}`, true)
+                .addField('Wind Speed', `${info.wind.speed} ${info.units.speed}`, true)
+                .addField('Humidity', `${info.atmosphere.humidity}%`, true)
+                .addField('Condition', info.item.condition.text, true);
+            message.channel.send(embed);
+        } else {
+            message.channel.send("No data!");
         }
-    };
-    request();
+    });
 };
 
 module.exports.help = {
